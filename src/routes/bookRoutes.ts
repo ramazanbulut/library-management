dd78b3691bdba4666cfb47495b04f838b25b7bb0
ramazanbulut <56506578+ramazanbulut@users.noreply.github.com>
2025-01-10 17:00:25 +0300

    validation

--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -1,10 +1,20 @@
 import express from 'express';
 import { listBooks, getBookDetails, createBook } from '../controllers/bookController';
+import { body } from 'express-validator';
+import { validate } from '../middleware/validate';
+
 
 const router = express.Router();
 
 router.get('/', listBooks);
 router.get('/:id', getBookDetails);
-router.post('/', createBook);
+router.post('/', [
+    body('name')
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage('Book name is required'),
+    validate
+], createBook);
+
 
 export default router;
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,18 @@
 import express from 'express';
 import { listUsers, getUserDetails, createUser } from '../controllers/userController';
+import { body } from 'express-validator';
+import { validate } from '../middleware/validate';
 
 const router = express.Router();
 
 router.get('/', listUsers);
 router.get('/:id', getUserDetails);
-router.post('/', createUser);
+router.post('/', [
+    body('name')
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage('Book name is required'),
+    validate
+], createUser);
 
 export default router;
